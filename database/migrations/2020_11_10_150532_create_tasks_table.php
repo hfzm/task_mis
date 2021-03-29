03017375cfc702086bd11095a6fa334f7d6c55a7
Hafizullah Masoudi <hafizullah.masoudi2508@gmail.com>
2021-03-30 01:41:58 +0430

    section 9

---
+++ database/migrations/2020_11_10_150532_create_tasks_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->unsigned();
+            $table->integer('parent_id')->nullable();
+            $table->integer('department_id')->unsigned();
+            $table->string('title');
+            $table->text('description');
+            $table->string('priority');
+            $table->string('start_date');
+            $table->string('end_date');
+            $table->integer('progress')->default('0');
+            $table->text('result')->nullable();
+            $table->string('file')->nullable();
+            $table->integer('status')->default('0');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('department_id')->references('id')->on('departments');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
